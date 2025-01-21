597fd7e6aabe94ce815edded47932ec44ebe2446
Jack Hodkinson <jackhodkinson93@gmail.com>
2025-01-21 16:48:01 +0000

    Refactor sample generation: Encapsulate logic in `generate_samples` function and update main execution flow

--- human_eval_infilling/generate_samples.py
+++ human_eval_infilling/generate_samples.py
@@ -1,21 +1,26 @@
-from human_eval_infilling.data import write_jsonl, read_problems
-
-problems = read_problems(benchmark_name="test")
+from human_eval_infilling.data import read_problems, write_jsonl
 
 
 def generate_one_completion(prompt, suffix):
     return "    return "
 
 
-num_samples_per_task = 1
-samples = [
-    dict(
-        task_id=task_id,
-        completion=generate_one_completion(
-            problems[task_id]["prompt"], problems[task_id]["suffix"]
-        ),
-    )
-    for task_id in problems
-    for _ in range(num_samples_per_task)
-]
-write_jsonl("./data/samples.jsonl", samples)
+def generate_samples(benchmark_name: str, num_samples_per_task: int):
+    problems = read_problems(benchmark_name=benchmark_name)
+    num_samples_per_task = 1
+    samples = [
+        dict(
+            task_id=task_id,
+            completion=generate_one_completion(
+                problems[task_id]["prompt"], problems[task_id]["suffix"]
+            ),
+        )
+        for task_id in problems
+        for _ in range(num_samples_per_task)
+    ]
+    write_jsonl("./data/samples.jsonl", samples)
+    return samples
+
+
+if __name__ == "__main__":
+    generate_samples("test", 1)
